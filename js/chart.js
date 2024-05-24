a2e752b9f70d866bbe9fd82171f6452d33b8b7f0
Omar Alexis Torbisco Pizarro <omar16354@gmail.com>
2024-05-23 19:34:06 -0500

    chart.js

--- js/chart.js
+++ js/chart.js
@@ -10,7 +10,45 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
+const state = new AppState();
+state.loadItems();
+console.log(state);
+
 function renderChart() {
+    const ctx = canvasElem.getContext('2d');
+    const productNames = state.allProducts.map(product => product.name);
+    const productVotes = state.allProducts.map(product => product.timesClicked);
+    const productShown = state.allProducts.map(product => product.timesShown);
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: productNames,
+            datasets: [
+                {
+                    label: 'Votos',
+                    data: productVotes,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Visto',
+                    data: productShown,
+                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                    borderColor: 'rgba(153, 102, 255, 1)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
 
 renderChart();
